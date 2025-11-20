c6f5b7aa0e926519638707d42b1263a347f45180
mUser01 <mimouser01@gmail.com>
2025-11-20 15:32:28 -0400

    Finish layout and colors of the project using CSS

--- BookLibrary.js
+++ BookLibrary.js
@@ -9,16 +9,16 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.isRead = this.read ? 'read' : 'not read yet'
+    this.isRead = this.read ? '✅read' : '❌not read yet'
 }
 
 Book.prototype.changeMyReadStatus = function() {
     this.read = !this.read;
-    this.isRead = this.read ? 'read' : 'not read yet'
+    this.isRead = this.read ? '✅read' : '❌not read yet'
 }
 
 Book.prototype.info = function () {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead}`
+        return `📕${this.title} 👤by ${this.author}, 📃${this.pages} pages, ${this.isRead}`
 }
 
 
@@ -35,18 +35,12 @@ function addBookToLibrary(title, author, pages, read) {
 
 Object.setPrototypeOf(addBookToLibrary.prototype, Book.prototype);
 
-
-
-addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
-addBookToLibrary('Martyr!', 'Kaveh Akbar', 350, true);
-addBookToLibrary('Good Material', 'Dolly Alderton', 336, false);
-addBookToLibrary('You Dreamed of Empires', 'Álvaro Enrigue', 240, true);
-addBookToLibrary('James', 'Percival Everett', 320, false);
-addBookToLibrary('All Fours', 'Miranda July', 400, true);
-addBookToLibrary('Everyone Who Is Gone Is Here', 'Jonathan Blitzer', 544, false);
-
 function loopBooks() {
+
+    // Create Book
     myLibrary.forEach(book => {
+        var divBook = document.createElement("div")
+        divBook.classList.add("book")
         var bookEl = document.createElement("p");
         bookEl.textContent = book.info();
 
@@ -65,9 +59,11 @@ function loopBooks() {
             loopBooks();
         });
 
-        bookEl.appendChild(deleteBook);
-        bookEl.appendChild(changeRead);
-        output.appendChild(bookEl);    
+        
+        divBook.appendChild(bookEl);
+        divBook.appendChild(deleteBook);
+        divBook.appendChild(changeRead);
+        output.appendChild(divBook);
     })
 }
 
@@ -95,6 +91,6 @@ dialog.addEventListener('close', (e) => {
 
 confirmBtn.addEventListener("click", (event) => {
     event.preventDefault();
-    addBookToLibrary(title.value, author.value, pages.value, read.value == true ? true : false);
+    addBookToLibrary(title.value, author.value, pages.value, read.value);
     dialog.close();
 })
